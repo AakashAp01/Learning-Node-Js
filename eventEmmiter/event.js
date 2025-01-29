0b5c688d8a8865857cf0dee48c99468348a04f6c
Aakash Prajapati <aakashap309@gmail.com>
2025-01-29 23:21:08 +0530

    Add event emitter functionality to track API call counts

---
+++ eventEmmiter/event.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const EventEmmiter = require('events');
+const eventEmmiter = new EventEmmiter();
+
+
+
+let count = 0
+
+eventEmmiter.on('called',()=>{
+    count++;
+    console.log(`API has been called ${count} times`);
+})
+
+
+app.get('/',(req,resp)=>{
+    eventEmmiter.emit('called')
+    resp.send(`Home API called. Total API Calls: ${count}`);
+})
+
+app.get('/search',(req,resp)=>{
+    eventEmmiter.emit('called')   
+    resp.send(`Search API called. Total API Calls: ${count}`);
+})
+
+app.listen(3000);
